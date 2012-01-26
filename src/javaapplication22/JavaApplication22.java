e170be172a7099f3298e9431c572468090ab6d1d
Tomato <oco.to.nusi@gmail.com>
2012-01-27 01:52:06 +0900

    test

--- src/javaapplication22/JavaApplication22.java
+++ src/javaapplication22/JavaApplication22.java
@@ -11,5 +11,6 @@ public class JavaApplication22 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("testかも");
     }
 }
